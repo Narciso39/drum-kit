9508e930d2d9de30523870bc7673070fd92603f6
luis <felipenarciso15@gmail.com>
2024-08-03 11:01:35 -0300

    projeto completo

---
+++ POO.js
@@ -0,0 +1,11 @@
+function HouseKeeper1 (yearsOfExperience, name, cleaningRepertorie) {
+    this.yearsOfExperience = yearsOfExperience;
+    this.name = name;
+    this.cleaningRepertorie = cleaningRepertorie;
+    this.clean = function () {
+        alert("cleaning in progress...");
+    }
+  }
+  
+  
+
